99c021140b48e12cf154ae961096f6ec5b6926a4
GasTrotteyn <ggtrotteyn@gmail.com>
2020-03-30 16:26:58 -0300

    registro y login funcionando

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,55 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const usuarios = require('./usuarios.json');
-
 app.use(cors({ origin: '*' }), bodyParser.json());
-
 app.listen(3001, () => {
     console.log('server running');
 });
-
-
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios',verificarUsuario, (req, res) => {
     const nuevoUsuario = req.body;
     usuarios.push(nuevoUsuario);
     console.log(JSON.stringify(usuarios));
     res.status(200).json(nuevoUsuario);
 });
-
 app.get('/usuarios', (req, res) => {
     res.json(usuarios);
-})
+});
+app.post('/usuarios/login',logIn, (req, res) => {
+    usuario = req.usuario;
+    usuario.logIn = true;
+    res.status(200).send('Acceso autorizado')
+});
+app.post('/usuarios/depositos', estaLogueado, (req, res) => {
+    usuario = req.usuario;
+    const monto = parseInt(req.body.monto) ;
+});
+///middleware usuario
+function verificarUsuario(req, res, next){
+    const nombreUsuario = req.body.usuario;
+    const usuario = usuarios.find(element =>
+        element.usuario === nombreUsuario);
+        if (!usuario){
+            next();
+        }else{
+            res.status(404).send('Usuario ya existe, elegi otro')
+        }
+}
+function logIn(req, res, next){
+    const nombreUsuario = req.body.usuario;
+    const usuario = usuarios.find(element => element.usuario === nombreUsuario)
+    const passwordRequerida = parseInt(req.body.password);
+    if (usuario && parseInt(usuario.password) === passwordRequerida){
+        req.usuario = usuario;
+        req.password = passwordRequerida;
+        next();
+    }else{
+        res.status(401).send('Algunos de los datos no son correctos')
+    }
+}
+function estaLogueado( req, res, next){
+    const usuario = req.usuario;
+    const logIn = usuario.login;
+    if(logIn){
+        next()
+    }
+}
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,7 +1,8 @@
-window.onload= cargaPagina;
+window.onload = cargaPagina;
 
+//// Enviar formulario de registro///////////////////
 
-async function sendForm(){
+async function sendForm() {
     event.preventDefault();
     const usuario = getUser();
     await postUsusario(usuario);
@@ -9,7 +10,7 @@ async function sendForm(){
     form.reset();
 };
 
-async function postUsusario(usuario){
+async function postUsusario(usuario) {
     fetch('http://localhost:3001/usuarios', {
         method: 'POST',
         headers: {
@@ -18,19 +19,41 @@ async function postUsusario(usuario){
         },
         body: JSON.stringify(usuario)
     })
-    .then(function(res) {
-        return res.json()
-    })
-    .then(function(res){
-        console.log(res)
-    })
-    .catch(function(res) {console.log(res)})
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (res) {
+            console.log(res);
+            mostrarExitoRegistro();
+        })
+        .catch(function (res) {
+            //console.log(res);
+            mostrarUsuarioYaExiste();
+        })
 };
 
-function getUser () {
+function mostrarExitoRegistro() {
+    let msjExito = document.createElement('div');
+    msjExito.className = 'msjExito';
+    msjExito.innerHTML = 'Usted se ha registrado correctamante!'
+    let mensajeRegistro = document.getElementById('mensajeRegistro');
+    mensajeRegistro.innerHTML = '';
+    mensajeRegistro.appendChild(msjExito);
+}
+
+function mostrarUsuarioYaExiste() {
+    let msjYaExiste = document.createElement('div');
+    msjYaExiste.className = 'msjExito';
+    msjYaExiste.innerHTML = 'Ese usuario ya existe!'
+    let mensajeRegistro = document.getElementById('mensajeRegistro');
+    mensajeRegistro.innerHTML = '';
+    mensajeRegistro.appendChild(msjYaExiste);
+}
+
+function getUser() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
-    const usuario= document.getElementById('usuarioRegistro').value;
+    const usuario = document.getElementById('usuarioRegistro').value;
     const password = document.getElementById('passwordRegistro').value;
     return {
         nombre: nombre,
@@ -40,10 +63,74 @@ function getUser () {
     }
 };
 
-function eventos(){
-    document.getElementById('formularioRegistro').addEventListener('submit', sendForm)
+/// enviar login ///////////////
+
+function getLogin() {
+    const usuario = document.getElementById('usuarioLogin').value;
+    const password = document.getElementById('passwordLogin').value;
+    return {
+        usuario: usuario,
+        password: password
+    }
+}
+
+function postLogin(usuarioLogueado) {
+    fetch('http://localhost:3001/usuarios/login',
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'aplication/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(usuarioLogueado)
+        })
+        .then(function (res) {
+            console.log(res.status);
+            if(res.status === 200){
+                mostrarExitoLogin();
+            }else{
+                mostrarErrorLogin();
+            }
+        })
+}
+
+async function sendLogin(event) {
+    event.preventDefault();
+    const usuarioLogueado = getLogin();
+    await postLogin(usuarioLogueado);
+    let form = document.getElementById('formularioRegistro');
+    form.reset();
+}
+
+function mostrarExitoLogin() {
+    let msjExito = document.createElement('div');
+    msjExito.className = 'msjExito';
+    msjExito.innerHTML = 'Usted se ha logueado correctamante!'
+    let mensajeLogin = document.getElementById('mensajeLogin');
+    mensajeLogin.innerHTML = '';
+    mensajeLogin.appendChild(msjExito);
+    document.getElementById('ventanaDeposito').style.display = 'block';
+    document.getElementById('ventanaTransferencias').style.display = 'block';
+}
+
+function mostrarErrorLogin() {
+    let msjExito = document.createElement('div');
+    msjExito.className = 'msjExito';
+    msjExito.innerHTML = 'El usuario o la contraseña son incorrectos'
+    let mensajeLogin = document.getElementById('mensajeLogin');
+    mensajeLogin.innerHTML = '';
+    mensajeLogin.appendChild(msjExito);
+}
+
+
+
+
+
+function eventos() {
+    document.getElementById('formularioRegistro').addEventListener('submit', sendForm);
+    document.getElementById('formularioLogin').addEventListener('submit', sendLogin)
 };
 
-function cargaPagina(){
+function cargaPagina() {
     eventos();
 }
