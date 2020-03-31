fce77b5abeec52fe506f1835f61d1967416019fd
GasTrotteyn <ggtrotteyn@gmail.com>
2020-03-31 11:48:09 -0300

    algnas cosas

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,9 @@ app.use(cors({ origin: '*' }), bodyParser.json());
 app.listen(3001, () => {
     console.log('server running');
 });
-app.post('/usuarios',verificarUsuario, (req, res) => {
+app.post('/usuarios', verificarUsuario, (req, res) => {
     const nuevoUsuario = req.body;
+    nuevoUsuario.saldo = 0;
     usuarios.push(nuevoUsuario);
     console.log(JSON.stringify(usuarios));
     res.status(200).json(nuevoUsuario);
@@ -16,42 +17,67 @@ app.post('/usuarios',verificarUsuario, (req, res) => {
 app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
-app.post('/usuarios/login',logIn, (req, res) => {
+app.get('/usuarios/:usuario', esUnUsuario, (req, res) => {
+    usuario = req.usuario;
+    res.json(usuario);
+});
+
+app.post('/usuarios/:usuario', logIn, (req, res) => {
     usuario = req.usuario;
     usuario.logIn = true;
     res.status(200).send('Acceso autorizado')
 });
-app.post('/usuarios/depositos', estaLogueado, (req, res) => {
+
+///////////////
+app.post('/usuarios/:usuario/depositos', esUnUsuario, (req, res) => {
     usuario = req.usuario;
-    const monto = parseInt(req.body.monto) ;
+    const saldo = parseInt(usuario.saldo);
+    const monto = parseInt(req.body.saldo);
+    const nuevoSaldo = saldo + monto;
+    usuario = req.nuevoSaldo;
+    //const nuevoSaldo = (saldo + monto);
+
+    res.status(200).json(usuario);
 });
 ///middleware usuario
-function verificarUsuario(req, res, next){
+function verificarUsuario(req, res, next) {
     const nombreUsuario = req.body.usuario;
     const usuario = usuarios.find(element =>
         element.usuario === nombreUsuario);
-        if (!usuario){
-            next();
-        }else{
-            res.status(404).send('Usuario ya existe, elegi otro')
-        }
+    if (!usuario) {
+        next();
+    } else {
+        res.status(404).send('Usuario ya existe, elegi otro')
+    }
 }
-function logIn(req, res, next){
+function logIn(req, res, next) {
     const nombreUsuario = req.body.usuario;
     const usuario = usuarios.find(element => element.usuario === nombreUsuario)
     const passwordRequerida = parseInt(req.body.password);
-    if (usuario && parseInt(usuario.password) === passwordRequerida){
+    if (usuario && parseInt(usuario.password) === passwordRequerida) {
         req.usuario = usuario;
         req.password = passwordRequerida;
         next();
-    }else{
+    } else {
         res.status(401).send('Algunos de los datos no son correctos')
     }
 }
-function estaLogueado( req, res, next){
-    const usuario = req.usuario;
-    const logIn = usuario.login;
-    if(logIn){
-        next()
+
+////////////
+function esUnUsuario(req, res, next) {
+    const nombreUsuario = req.params.usuario;
+    const usuario = usuarios.find(element => element.usuario === nombreUsuario)
+    if (usuario) {
+        req.usuario = usuario;
+        next();
+    } else {
+        res.status(404).send('Usuario no registrado')
     }
 }
+// function estaLogueado( req, res, next){
+//     const usuario = req.usuario;
+//     const logIn = usuario.login;
+//     if(logIn){
+//         next()
+//     }
+// }
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,7 +1,7 @@
 window.onload = cargaPagina;
-
+​
 //// Enviar formulario de registro///////////////////
-
+​
 async function sendForm() {
     event.preventDefault();
     const usuario = getUser();
@@ -9,7 +9,7 @@ async function sendForm() {
     let form = document.getElementById('formularioRegistro');
     form.reset();
 };
-
+​
 async function postUsusario(usuario) {
     fetch('http://localhost:3001/usuarios', {
         method: 'POST',
@@ -31,25 +31,30 @@ async function postUsusario(usuario) {
             mostrarUsuarioYaExiste();
         })
 };
-
+​
 function mostrarExitoRegistro() {
     let msjExito = document.createElement('div');
-    msjExito.className = 'msjExito';
+    msjExito.className = 'mjeExito';
     msjExito.innerHTML = 'Usted se ha registrado correctamante!'
     let mensajeRegistro = document.getElementById('mensajeRegistro');
-    mensajeRegistro.innerHTML = '';
+    //mensajeRegistro.innerHTML = '';
+    mensajeRegistro.style.display = 'block';
+    document.getElementById('registro').style.display = 'none';
     mensajeRegistro.appendChild(msjExito);
 }
-
+​
 function mostrarUsuarioYaExiste() {
+​
     let msjYaExiste = document.createElement('div');
     msjYaExiste.className = 'msjExito';
     msjYaExiste.innerHTML = 'Ese usuario ya existe!'
     let mensajeRegistro = document.getElementById('mensajeRegistro');
-    mensajeRegistro.innerHTML = '';
-    mensajeRegistro.appendChild(msjYaExiste);
+    //mensajeRegistro.innerHTML = '';
+    mensajeRegistro.style.display = 'block';
+    document.getElementById('registro').style.display = 'none';
+    mensajeRegistro.insertAdjacentElement('afterbegin', msjYaExiste);
 }
-
+​
 function getUser() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
@@ -62,10 +67,11 @@ function getUser() {
         password: password
     }
 };
-
+​
 /// enviar login ///////////////
-
+​
 function getLogin() {
+​
     const usuario = document.getElementById('usuarioLogin').value;
     const password = document.getElementById('passwordLogin').value;
     return {
@@ -73,7 +79,7 @@ function getLogin() {
         password: password
     }
 }
-
+​
 function postLogin(usuarioLogueado) {
     fetch('http://localhost:3001/usuarios/login',
         {
@@ -93,26 +99,30 @@ function postLogin(usuarioLogueado) {
             }
         })
 }
-
+​
 async function sendLogin(event) {
     event.preventDefault();
     const usuarioLogueado = getLogin();
     await postLogin(usuarioLogueado);
-    let form = document.getElementById('formularioRegistro');
+    let form = document.getElementById('formularioLogin');
     form.reset();
 }
-
+​
 function mostrarExitoLogin() {
     let msjExito = document.createElement('div');
     msjExito.className = 'msjExito';
     msjExito.innerHTML = 'Usted se ha logueado correctamante!'
     let mensajeLogin = document.getElementById('mensajeLogin');
-    mensajeLogin.innerHTML = '';
-    mensajeLogin.appendChild(msjExito);
-    document.getElementById('ventanaDeposito').style.display = 'block';
-    document.getElementById('ventanaTransferencias').style.display = 'block';
+    //mensajeLogin.innerHTML = '';
+    mensajeLogin.insertAdjacentElement('afterbegin', msjExito);
+​
+    document.getElementById('formularioLogin').style.display = 'none';
+    document.getElementById('mensajeLogin').style.display = 'block'
+    
+    //document.getElementById('ventanaDeposito').style.display = 'block';
+    //document.getElementById('ventanaTransferencias').style.display = 'block';
 }
-
+​
 function mostrarErrorLogin() {
     let msjExito = document.createElement('div');
     msjExito.className = 'msjExito';
@@ -121,16 +131,50 @@ function mostrarErrorLogin() {
     mensajeLogin.innerHTML = '';
     mensajeLogin.appendChild(msjExito);
 }
-
-
-
-
-
+​
+function ingresarApp(){
+    let portada = document.getElementById('portada');
+    let formularioLogin = document.getElementById('formularioIngresar');
+    portada.style.display = 'none';
+    formularioLogin.style.display = 'block'
+}
+​
+function registrarse(){
+    let portada = document.getElementById('portada');
+    let formRegistro = document.getElementById('registro');
+    portada.style.display = 'none';
+    formRegistro.style.display = 'block';
+    document.getElementById('mensajeRegistro').style.display = 'none';
+}
+​
+function depositar () {
+    document.getElementById('mensajeLogin').style.display = 'none';
+    document.getElementById('ventanaDeposito').style.display = 'block';
+}
+​
+function transferir() {
+    document.getElementById('mensajeLogin').style.display = 'none';
+    document.getElementById('ventanaTransferencias').style.display = 'block'
+}
 function eventos() {
     document.getElementById('formularioRegistro').addEventListener('submit', sendForm);
-    document.getElementById('formularioLogin').addEventListener('submit', sendLogin)
+    document.getElementById('formularioLogin').addEventListener('submit', sendLogin);
+    document.getElementById('ingresar').addEventListener('click', ingresarApp);
+    document.getElementById('registrar').addEventListener('click', registrarse);
+    document.getElementById('depositar').addEventListener('click', depositar);
+    document.getElementById('transferir').addEventListener('click', transferir);
+    document.getElementById('registrarme').addEventListener('click', registrarse )
 };
-
+​
 function cargaPagina() {
     eventos();
+}
+​
+function esUnUsuario(req, res, next){
+    const nombreUsuario = req.params.usuario;
+    const usuario = usuarios.find(element => element.usuario === nombreUsuario)
+    if(usuario){
+        req.usuario = usuario;
+        next();
+    }
 }
