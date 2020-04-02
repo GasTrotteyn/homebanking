730c7899a6901fcd87a66ae2786bf09116655ac5
fer <fer_cappelletti@hotmail.com>
2020-04-02 15:52:11 -0300

    transferecncias ok

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.post('/usuarios', verificarUsuario, (req, res) => {
     const nuevoUsuario = req.body;
     nuevoUsuario.saldo = 0;
     usuarios.push(nuevoUsuario);
-    console.log(JSON.stringify(usuarios));
+    //console.log(JSON.stringify(usuarios));
     res.status(200).json(nuevoUsuario);
 });
 app.get('/usuarios', (req, res) => {
@@ -32,13 +32,27 @@ app.post('/usuarios/login', logIn, (req, res) => {
 ///////////////
 app.post('/usuarios/:usuario/depositos', esUnUsuario, async function (req, res) {
     usuario = req.usuario;
-    console.log(req.body);
+    //console.log(req.body);
     const monto = parseInt(req.body.monto);
     usuario.saldo = parseInt(usuario.saldo) + monto;
     let enviar = await JSON.stringify({ saldo: usuario.saldo })
     res.status(200).send(enviar);
 });
 
+app.post('/usuarios/transferencias', sonUsuarios, tieneSaldo, async function (req, res) {
+    usuarioEmisor = req.usuarioEmisor;
+    usuarioReceptor = req.usuarioReceptor;
+
+    let monto = parseInt(req.body.monto);
+    usuarioEmisor.saldo = parseInt(usuarioEmisor.saldo) - monto;
+    usuarioReceptor.saldo = parseInt(usuarioReceptor.saldo) + monto;
+
+    res.status(200).send({
+        saldoEmisor: usuarioEmisor.saldo,
+        saldoReceptor: usuarioReceptor.saldo
+    })
+
+})
 ///middleware usuario
 function verificarUsuario(req, res, next) {
     const nombreUsuario = req.body.usuario;
@@ -70,16 +84,41 @@ function esUnUsuario(req, res, next) {
     const usuario = usuarios.find(element => element.usuario === nombreUsuario)
     if (usuario) {
         req.usuario = usuario;
-        //req.saldo = saldo
         next();
     } else {
         res.status(404).send('Usuario no registrado')
     }
 }
-// function estaLogueado( req, res, next){
-//     const usuario = req.usuario;
-//     const logIn = usuario.login;
-//     if(logIn){
-//         next()
-//     }
-// }
+/////////midd verificar los dos son usuarios
+function sonUsuarios(req, res, next) {
+    let emisor = req.body.usuario;
+    const usuarioEmisor = usuarios.find(element => element.usuario === emisor)
+
+    let receptor = req.body.receptor;
+    const usuarioReceptor = usuarios.find(element => element.usuario === receptor)
+    //console.log(usuarioReceptor)
+    if (usuarioEmisor && usuarioReceptor) {
+        req.usuarioEmisor = usuarioEmisor
+        //console.log(usuarioEmisor)
+        req.usuarioReceptor = usuarioReceptor;
+        next();
+    } else {
+        res.status(401).send('Alguno de los usuarios no es correcto')
+    }
+}
+
+function tieneSaldo(req, res, next) {
+    usuarioEmisor = req.usuarioEmisor
+    let saldo = parseInt(usuarioEmisor.saldo);
+    //console.log(saldo)
+    let monto = parseInt(req.body.monto);
+    console.log(monto)
+    if (saldo > monto) {
+        req.monto = monto
+        next();
+    }else{
+        res.status(201).send('No te alcanza cariño')
+    }
+}
+
+
