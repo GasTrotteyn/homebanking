38d4dc0009acb714682c7b76d544d996f55a0390
fer <fer_cappelletti@hotmail.com>
2020-04-09 18:10:29 -0300

    conectando a base de datos MongoDB

--- backend/controllers/usuario-controller.js
+++ backend/controllers/usuario-controller.js
@@ -1,54 +1,67 @@
-//let usuarios = require("../usuarios.json");
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const usuario = new Schema({
-    nombre: String,
-    apellido: String,
-    usuario: String,
-    password: Number,
-    saldo: Number
-})
-
-const Usuarios = mongoose.model('Usuarios', usuario);
+const Usuarios = require('../models/models');
 
 function postUsuario(req, res) {
     const nuevoUsuario = req.body;
     objetoUsuario = new Usuarios(nuevoUsuario);
-    //nuevoUsuario.saldo = 0;
-    //usuarios.push(nuevoUsuario);
-    objetoUsuario.save().then((resultado) => {
-        res.status(200).send()
-    })
+    objetoUsuario.save()
+        .then(async function (objetoUsuario) {
+            objetoUsuario_id = objetoUsuario.dni;
+            await objetoUsuario.save()
+            console.log(objetoUsuario_id)
+            res.status(200).send()
+        })
         .catch((error) => {
             res.status(500).send()
         })
 }
 
 function getUsuarios(req, res) {
-    res.json(usuarios);
+    Usuarios.find()
+        .then((usuarios) => {
+            res.json(usuarios);
+        })
+        .catch((err) => {
+            res.status(500).send()
+        })
 }
-
+/*
+Trae usuario por el nombre de usuario pero no se utiliza en ninguna ruta
 function getUser(req, res) {
-    usuario = req.usuario;
-    res.json(usuario);
+    const usuario = req.usuario;
+
+    Usuarios.findOne({'usuario' : usuario}, (err, usuario) => {
+        if(usuario){
+            res.status(200).send('ok')
+        }else{
+            res.status(401).send('not ok')
+        }
+    })
 }
+*/
 
 function postLogin(req, res) {
     res.status(200).json(req.token);
 }
 
-function getUserFromDB(user) {
-    const encontrado = usuarios.find(element => element.usuario === user);
-    return encontrado
+function getUserFromDB(usuario) {
+    Usuarios.find({'usuario': usuario})
+    .then((user) => {
+        //console.log(user)
+        return user
+    })
 }
 
 function postDeposito(req, res) {
     usuarioSalidoDelToken = req.usuario;
     const monto = parseInt(req.body.monto);
-    usuario = getUserFromDB(usuarioSalidoDelToken);
+    let usuario = getUserFromDB(usuarioSalidoDelToken);
+    console.log(usuario)
     usuario.saldo = parseInt(usuario.saldo) + monto;
-    let enviar = JSON.stringify({ saldo: usuario.saldo });
-    res.status(200).send(enviar);
+    usuario.save()
+    .then((usuario) => {
+        let enviar = JSON.stringify({ saldo: usuario.saldo });
+        res.status(200).send(enviar);
+    })
 }
 function postTransferencia(req, res) {
     let usuarioEmisor = req.usuarioEmisor;
@@ -67,8 +80,7 @@ function postTransferencia(req, res) {
 module.exports = {
     postUsuario,
     getUsuarios,
-    getUser,
     postLogin,
     postDeposito,
-    postTransferencia
+    postTransferencia,
 };
